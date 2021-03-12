42fcc419dca32f562c2a2e3d91d50c32ec3f82d0
Taras Radchenko <petuzk.dev@gmail.com>
2021-03-12 16:13:13 +0100

    Add Item class

--- Knapsack/Program.cs
+++ Knapsack/Program.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 
+
+
 namespace Knapsack
 {
+    class Item
+    {
+        public int Weight { get; set; }
+        public int Value { get; set; }
+
+        public Item(int weight, int value)
+        {
+            Weight = weight;
+            Value = value;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Item i = new Item(20, 30);
             Console.WriteLine("Hello World!");
         }
     }
