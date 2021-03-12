bda9fd53c2f415e10b66be66dd0ee768814c3fee
Taras Radchenko <petuzk.dev@gmail.com>
2021-03-12 20:44:23 +0100

    Improve algorithm & create main()

--- Knapsack/Program.cs
+++ Knapsack/Program.cs
@@ -16,30 +16,52 @@ namespace Knapsack
             Weight = weight;
             Value = value;
         }
+
+        public override string ToString()
+        {
+            return String.Format("Item(W={0}, V={1}, k={2})", Weight, Value, (double) Value/Weight);
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Item i = new Item(20, 30);
-            Console.WriteLine("Hello World!");
+            var seed = DateTimeOffset.Now.ToUnixTimeSeconds();
+            Console.WriteLine("Seed: {0}", seed);
+
+            var items = RandomizeItems(10, seed);
+            Console.WriteLine("Items:");
+            Console.WriteLine("  " + String.Join("\n  ", items));
+
+            var knapsack = GetKnapsackContents(50, items);
+            Console.WriteLine("Knapsack:");
+            Console.WriteLine("  " + String.Join("\n  ", knapsack));
         }
 
         static List<Item> RandomizeItems(int numItems, long? rng_seed = null)
         {
-            RandomNumberGenerator rng = new RandomNumberGenerator(rng_seed);
+            var rng = new RandomNumberGenerator(rng_seed);
 
             return Enumerable.Range(0, numItems)
                 .Select(i => new Item(rng.nextInt(1, 29), rng.nextInt(1, 29)))
                 .ToList();
         }
 
-        static List<Item> GetKnapsackContents(long capacity, IEnumerable<Item> items)
+        static List<Item> GetKnapsackContents(int capacity, IEnumerable<Item> items)
         {
+            bool ItemShouldBeAdded(Item item) {
+                if (item.Weight <= capacity) {
+                    capacity -= item.Weight;
+                    return true;
+                }
+                return false;
+            }
+
             return items
-                .OrderBy(item => item.Value / item.Weight)
-                .TakeWhile(item => (capacity -= item.Weight) >= 0)
+                // Sort by value/weight in reverse order (descending)
+                .OrderBy(item => (double) item.Weight / item.Value)
+                .Where(ItemShouldBeAdded)
                 .ToList();
         }
     }
