bc7d9a6d5c6be29de3e888971d95f0af1328491a
Taras Radchenko <petuzk.dev@gmail.com>
2021-03-13 21:08:25 +0100

    App is now working as required

--- Knapsack/Program.cs
+++ Knapsack/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Globalization;
 
 
 
 namespace Knapsack
 {
-    class Item
+    public class Item
     {
         public int Weight { get; set; }
         public int Value { get; set; }
@@ -16,30 +17,100 @@ namespace Knapsack
             Weight = weight;
             Value = value;
         }
-
-        public override string ToString()
-        {
-            return String.Format("Item(W={0}, V={1}, k={2})", Weight, Value, (double) Value/Weight);
-        }
     }
 
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            var seed = DateTimeOffset.Now.ToUnixTimeSeconds();
-            Console.WriteLine("Seed: {0}", seed);
+            int numItems = 10;
+            int capacity = 50;
+            long seed = DateTimeOffset.Now.ToUnixTimeSeconds();
 
-            var items = RandomizeItems(10, seed);
-            Console.WriteLine("Items:");
-            Console.WriteLine("  " + String.Join("\n  ", items));
+            if (!ParseArgs(args, ref numItems, ref capacity, ref seed))
+                return 1;
 
-            var knapsack = GetKnapsackContents(50, items);
-            Console.WriteLine("Knapsack:");
-            Console.WriteLine("  " + String.Join("\n  ", knapsack));
+            var items = RandomizeItems(numItems, seed);
+            var taken = GetKnapsackContents(capacity, items);
+
+            Console.WriteLine("Ziarno: {0}", seed);
+            Console.WriteLine("Pojemność: {0}", capacity);
+
+            var tableWriter = new ResultTableWriter(new ResultTableWriter.LangPL());
+            tableWriter.WriteHeader();
+            items.ForEach(item => tableWriter.WriteRow(item, taken.Contains(item)));
+            tableWriter.WriteFooter();
+
+            return 0;
         }
 
-        static List<Item> RandomizeItems(int numItems, long? rng_seed = null)
+        static bool ParseArgs(string[] args, ref int numItems, ref int capacity, ref long seed)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i];
+
+                if (arg == "-h" || arg == "--help")
+                {
+                    Console.WriteLine(
+                        "usage: knapsack "+
+                        "[-h | --help] "+
+                        "[(-n | --num-items) n] "+
+                        "[(-c | --capacity) c] "+
+                        "[(-s | --seed) s]"
+                    );
+                    return false;
+                }
+
+                int skipChars;
+
+                if (arg.StartsWith("--"))     skipChars = 2;
+                else if (arg.StartsWith("-")) skipChars = 1;
+                else
+                {
+                    Console.WriteLine("expected argument name, got '{0}'", arg);
+                    return false;
+                }
+
+                if (++i >= args.Length)
+                {
+                    Console.WriteLine("expected value for '{0}'", arg);
+                    return false;
+                }
+
+                bool isParsed;
+                string value = args[i];
+
+                switch (skipChars, arg.Substring(skipChars))
+                {
+                    case (1, "n"):
+                    case (2, "num-items"):
+                        isParsed = int.TryParse(value, NumberStyles.None, null, out numItems);
+                        break;
+                    case (1, "c"):
+                    case (2, "capacity"):
+                        isParsed = int.TryParse(value, NumberStyles.None, null, out capacity);
+                        break;
+                    case (1, "s"):
+                    case (2, "seed"):
+                        isParsed = long.TryParse(value, NumberStyles.None, null, out seed);
+                        break;
+                    default:
+                        Console.WriteLine("unknown argument '{0}'", arg);
+                        return false;
+                }
+
+                if (!isParsed)
+                {
+                    Console.WriteLine("expected numeric value for '{0}', got '{1}'", arg, value);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static List<Item> RandomizeItems(int numItems, long rng_seed)
         {
             var rng = new RandomNumberGenerator(rng_seed);
 
