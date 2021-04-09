f1bf3c7d2f6ce9927b4598c6a69ccb6baf9642ae
Taras Radchenko <petuzk.dev@gmail.com>
2021-04-09 10:50:12 +0200

    Add RequestedAction and arguments parsing

--- DatabaseApp/Program.cs
+++ DatabaseApp/Program.cs
@@ -2,6 +2,9 @@
 using System.Net;
 using System.IO;
 using System.Data;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
 using Newtonsoft.Json;
 
 
@@ -42,9 +45,110 @@ namespace CurrencyApp
         }
     }
 
+    public class RequestedAction
+    {
+        public enum RAType
+        {
+            FETCH,
+            SHOW,
+        }
+
+        public readonly RAType Type;
+        public readonly DateTime Start;
+        public readonly DateTime? End;
+
+        public RequestedAction(RAType type, DateTime start, DateTime? end = null) {
+            Type = type;
+            Start = start;
+            End = end;
+        }
+
+        public override String ToString()
+        {
+            var type = Type == RAType.FETCH ? "FETCH" : "SHOW";
+            if (End == null) {
+                return String.Format("RequestedAction({0}, {1})", type, Start.ToString("d"));
+            } else {
+                return String.Format("RequestedAction({0}, {1}, {2})", type, Start.ToString("d"), ((DateTime) End).ToString("d"));
+            }
+        }
+    }
+
     public class Program
     {
-        public static void Main(string[] args)
+        public static List<RequestedAction> ParseArgs(string[] args)
+        {
+            int index = 0;
+            List<RequestedAction> req_actions = new();
+
+            try {
+                while (index < args.Length)  // TODO: out of index range
+                {
+                    if (args[index] == "show" && args[index+1] == "it") {
+                        if (req_actions.Count() == 0 || req_actions.Last().Type != RequestedAction.RAType.FETCH) {
+                            throw new ArgumentException(String.Format(
+                                "'it' must reference arguments of previous fetch (in 'show', arg #{0})", index + 1));
+                        }
+
+                        req_actions.Add(new(RequestedAction.RAType.SHOW, req_actions.Last().Start, req_actions.Last().End));
+                        index += 2;
+                    }
+                    else if (args[index] == "fetch" || args[index] == "show") {
+                        DateTime startDate;
+                        DateTime? endDate = null;
+
+                        // Parse first arg, which is required
+                        try {
+                            startDate = DateTime.Parse(args[index+1]);
+                        }
+                        catch (FormatException) {
+                            throw new ArgumentException(String.Format(
+                                "'{0}' is not a valid date (in '{1}', arg #{2})",
+                                args[index+1], args[index], index + 2));
+                        }
+
+                        // Parse second arg, which is optional
+                        if (index + 2 < args.Length) {
+                            try {
+                                endDate = DateTime.Parse(args[index+2]);
+                            }
+                            catch (FormatException) { }
+                        }
+
+                        var req_type = (args[index] == "fetch") ? RequestedAction.RAType.FETCH : RequestedAction.RAType.SHOW;
+                        req_actions.Add(new(req_type, startDate, endDate));
+                        index += 2 + (endDate != null ? 1 : 0);
+                    }
+                    else {
+                        throw new ArgumentException(String.Format("unknown action '{0}' (arg #{1})", args[index], index + 1));
+                    }
+                }
+            }
+            catch (IndexOutOfRangeException) {
+                throw new ArgumentException(String.Format("not enough arguments for action '{0}' (arg #{1})", args[index], index + 1));
+            }
+
+            return req_actions;
+        }
+
+        public static int Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = new CultureInfo("pl-PL", true);
+            try {
+                var req_actions = ParseArgs(args);
+            }
+            catch (ArgumentException e) {
+                Console.WriteLine(e.Message);
+                return 1;
+            }
+            foreach (var ra in ParseArgs(args)) {
+                Console.WriteLine(ra);
+            }
+
+            return 0;
+        }
+
+        public static void NotMain(string[] args)
         {
             DownloadRates myRates = new DownloadRates();
             DownloadRates deserializedProduct = JsonConvert.DeserializeObject<DownloadRates>(myRates.getRates());
