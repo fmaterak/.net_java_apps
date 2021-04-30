9bec15857386cf58d58cab634ab1896fc8f3a382
fmaterak <62309735+fmaterak@users.noreply.github.com>
2021-04-30 02:49:26 +0200

    Add files via upload

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
 
 namespace WebApp
 {
@@ -24,6 +26,9 @@ namespace WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<WebAppContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebAppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
